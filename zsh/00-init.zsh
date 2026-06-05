db4b164e9af45ea9b52dc79b176533e7618cca65
Brandon🔥 <brandonluth@gmail.com>
2026-06-05 13:33:46 -0700

    perf: Skip noisy navigation commands in shell history

--- zsh/00-init.zsh
+++ zsh/00-init.zsh
@@ -17,3 +17,13 @@ export GOPATH="$HOME/workspace/go"
 export SSH_KEY_PATH="~/.ssh/rsa_id"
 export ZSH="$HOME/.zsh/omz"
 export ZSH_COMPDUMP=$HOME/.cache/.zcompdump-$HOST
+
+# Skip noisy commands in zsh history (Atuin has matching filters in config.toml).
+zshaddhistory() {
+  emulate -L zsh
+  local line="${1%%$'\n'}"
+  [[ "$line" =~ '^[[:space:]]*(ls|cd|pwd|exit|clear)([[:space:]]|$)' ]] && return 1
+  return 0
+}
+
+export ZSH_AUTOSUGGEST_HISTORY_IGNORE='?(#c1,)(ls|cd|pwd|exit|clear)*'
